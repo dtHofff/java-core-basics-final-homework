73c82f53bdca9519882fa5d3b80f911382a069f5
Dmitriy <dmitriybru@gmail.com>
2025-04-12 17:03:03 +0300

    Add files via upload

--- src/main/java/school/sorokin/javacore/basics/Main.java
+++ src/main/java/school/sorokin/javacore/basics/Main.java
@@ -3,6 +3,8 @@ package school.sorokin.javacore.basics;
 public class Main {
     public static void main(String[] args) {
 
-    }
+        Contacts contacts = new Contacts();
 
+        while (true) contacts.showMenu();
+    }
 }
